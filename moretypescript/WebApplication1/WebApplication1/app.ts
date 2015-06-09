2bb0b5ece920eb16539c63b794a461c4ca5a6b8a
Pierre Nallet <pierrenallet@hotmail.com>
2015-06-09 11:19:38 -0700

    First commit

---
+++ moretypescript/WebApplication1/WebApplication1/app.ts
@@ -0,0 +1,35 @@
+﻿module MyMath {
+    export class Calculator {
+        constructor() {
+            var button: any;
+            button = document.querySelector("#compute");
+            var aInput: any = document.querySelector("#a");
+            var bInput: any = document.querySelector("#b");
+            var resultInput: any = document.querySelector("#result");
+            button.onclick = () => {
+                this.a = parseFloat(aInput.value);
+                this.b = parseFloat(bInput.value);
+                resultInput.value = this.sum();
+            }
+        }
+
+        a: number;
+        b: number;
+
+        sum() {
+            return this.a + this.b;
+        }
+    }
+}
+
+module MyMath {
+    export class OtherClass
+    {
+        doSomethingElse(){}
+    
+}
+}
+
+window.onload = function() {
+    window['calculator'] = new MyMath.Calculator();
+}
